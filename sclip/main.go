4784f49b6887443c19ff548f04ea656cec6d06f0
Nikhil Sahukar <nsahukar@gmail.com>
2020-12-10 22:07:40 +0530

    Now taking remote machine ip addr's trailing bit as an argument

--- sclip/main.go
+++ sclip/main.go
@@ -3,10 +3,20 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 	"os/exec"
 )
 
 func main() {
+	// Get trailing bit of the ip address from command line arguments
+	// If does not exists, use default.
+	ipTrailBit := 9
+	if len(os.Args) > 1 {
+		ipTrailBit = int(os.Args[1])
+	}
+	remoteMachineAddr := fmt.Sprintf("nix@192.168.1.%d", ipTrailBit)
+	fmt.Fprintln("SSHing to %s ...", remoteMachineAddr)
+
 	// Get most recent clipboard text from source machine
 	// In this case, source machine is strictly a Mac OSX.
 	cmdEchoClip := exec.Command("pbpaste", ">&1")
@@ -15,7 +25,6 @@ func main() {
 		log.Fatalln(err)
 	}
 
-	remoteMachineAddr := "nix@192.168.1.9"
 	remoteClipFilePath := "/home/nix/Public/clip.txt"
 	// saving the source machine clipboard text to a file on remote machine
 	// AND copy source machine clipboard text to remote machine clipboard
