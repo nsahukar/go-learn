c4010714093131699d03e4d308e70c53d95a5828
Nikhil Sahukar <nsahukar@gmail.com>
2021-01-05 12:57:55 +0530

    Using HandleFunc method of ServeMux instead of Handle

--- serve-http-mux/main.go
+++ serve-http-mux/main.go
@@ -5,31 +5,23 @@ import (
 	"net/http"
 )
 
-type fido int
-type hotdog int
-type hotcat int
-
-func (f fido) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func fido(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "Hello")
 }
 
-func (d hotdog) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func dog(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "Woof!")
 }
 
-func (c hotcat) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func cat(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "Meow...")
 }
 
 func main() {
-	var f fido
-	var d hotdog
-	var c hotcat
-
 	mux := http.NewServeMux()
-	mux.Handle("/", f)
-	mux.Handle("/dog", d)
-	mux.Handle("/cat", c)
+	mux.HandleFunc("/", fido)
+	mux.HandleFunc("/dog", dog)
+	mux.HandleFunc("/cat", cat)
 
 	http.ListenAndServe(":8080", mux)
 }
