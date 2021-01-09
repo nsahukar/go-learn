afe55606646641062b599921f3fb1d99eafa3a19
Nikhil Sahukar <nsahukar@gmail.com>
2021-01-09 11:24:32 +0530

    Using io.Copy to serve file on HTTP

--- serve-file-http/main.go
+++ serve-file-http/main.go
@@ -3,14 +3,28 @@ package main
 import (
 	"io"
 	"net/http"
+	"os"
 )
 
 func solo(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	io.WriteString(w, `<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/66/The_Leaning_Tower_of_Pisa_SB.jpeg/1200px-The_Leaning_Tower_of_Pisa_SB.jpeg">`)
+	io.WriteString(w, `<img src="pisa.jpeg">`)
+}
+
+func pisa(w http.ResponseWriter, r *http.Request) {
+	file, err := os.Open("pisa.jpeg")
+	if err != nil {
+		http.Error(w, "File Not Found", 404)
+		return
+	}
+	defer file.Close()
+
+	// using io.Copy to write file to http.ResponseWriter
+	io.Copy(w, file)
 }
 
 func main() {
 	http.HandleFunc("/", solo)
+	http.HandleFunc("/pisa.jpeg", pisa)
 	http.ListenAndServe(":8080", nil)
 }
