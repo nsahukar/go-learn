607e854afe530d936a8f0aedc1cbeaca876a7d2c
Nikhil Sahukar <nsahukar@gmail.com>
2021-04-07 14:08:38 +0530

    du-chan - added concurrency with multiple goroutines, channels and counting semaphore

--- du-chan/main.go
+++ du-chan/main.go
@@ -6,10 +6,18 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"sync"
+	"time"
 )
+
+// sema is a counting semaphore for limiting concurrency in dirents
+var sema = make(chan struct{}, 20)
 
 // dirents returns the entries of directory dir
 func dirents(dir string) []os.FileInfo {
+	sema <- struct{}{}        // acquire token
+	defer func() { <-sema }() // release token
+
 	entries, err := ioutil.ReadDir(dir)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "du %v\n", err)
@@ -20,11 +28,13 @@ func dirents(dir string) []os.FileInfo {
 
 // walkDir recursively walks the file tree rooted at dir
 // and sends the size of each found file on fileSizes.
-func walkDir(dir string, fileSizes chan<- int64) {
+func walkDir(dir string, n *sync.WaitGroup, fileSizes chan<- int64) {
+	defer n.Done()
 	for _, entry := range dirents(dir) {
 		if entry.IsDir() {
+			n.Add(1)
 			subdir := filepath.Join(dir, entry.Name())
-			walkDir(subdir, fileSizes)
+			go walkDir(subdir, n, fileSizes)
 		} else {
 			fileSizes <- entry.Size()
 		}
@@ -35,6 +45,8 @@ func printDiskUsage(nfiles, nbytes int64) {
 	fmt.Printf("%d files %.1f GB\n", nfiles, float64(nbytes)/1e9)
 }
 
+var verbose = flag.Bool("v", false, "show verbose progress messages")
+
 func main() {
 	// Determining the intial directories
 	flag.Parse()
@@ -43,20 +55,50 @@ func main() {
 		roots = []string{"."}
 	}
 
-	// Traverse the file tree.
+	// Traverse each root of the file tree in parallel.
 	fileSizes := make(chan int64)
+	var n sync.WaitGroup
+	for _, root := range roots {
+		n.Add(1)
+		go walkDir(root, &n, fileSizes)
+	}
+
 	go func() {
-		for _, root := range roots {
-			walkDir(root, fileSizes)
-		}
+		n.Wait()
 		close(fileSizes)
 	}()
 
-	// Print the results.
-	var nfiles, nbytes int64
-	for size := range fileSizes {
-		nfiles++
-		nbytes += size
+	// Traverse the file tree (one goroutine).
+	//	fileSizes := make(chan int64)
+	//	go func() {
+	//		for _, root := range roots {
+	//			walkDir(root, fileSizes)
+	//		}
+	//		close(fileSizes)
+	//	}()
+
+	// Print the results periodically.
+	var tick <-chan time.Time
+	if *verbose {
+		tick = time.Tick(500 * time.Millisecond)
 	}
+	var nfiles, nbytes int64
+loop:
+	for {
+		select {
+		case size, ok := <-fileSizes:
+			if !ok {
+				break loop
+			}
+			nfiles++
+			nbytes += size
+		case <-tick:
+			printDiskUsage(nfiles, nbytes)
+		}
+	}
+	//	for size := range fileSizes {
+	//		nfiles++
+	//		nbytes += size
+	//	}
 	printDiskUsage(nfiles, nbytes)
 }
